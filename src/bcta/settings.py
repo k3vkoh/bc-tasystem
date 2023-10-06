7bbfbdb99290da9db983d092ccdc4011c2d9b27b
Andy Deng <denga@bc.edu>
2023-10-06 16:56:16 -0400

    added basic signup functionality

--- src/bcta/settings.py
+++ src/bcta/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # own apps
     "users",
+    "main"
 ]
 
 MIDDLEWARE = [
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
