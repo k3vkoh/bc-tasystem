c817f5d64dc3b0d0b09424ce78ae6df4ce436ac0
k3vkoh <kev33koh@gmail.com>
2023-10-07 00:43:10 -0700

    urls changed and url added to navbar

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UploadView
 
 urlpatterns = [
-    path('', UploadView.as_view(), name='upload-excel'),
+    path('upload', UploadView.as_view(), name='upload-excel'),
 ]
