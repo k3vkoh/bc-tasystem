bc3fcaa5324896d2c6a83e43055dd1838aa1d6f4
k3vkoh <kev33koh@gmail.com>
2023-10-07 00:15:20 -0700

    added urls for courses

--- src/bcta/urls.py
+++ src/bcta/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", include("main.urls")),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")), 
+    path("courses/", include("courses.urls")),
 ]
