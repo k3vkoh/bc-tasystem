78ff14f5ea2298d6c2175ca4a4b072e89ef8e411
k3vkoh <kev33koh@gmail.com>
2023-10-07 00:10:30 -0700

    course models created

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from users.models import CustomUser
+from django.urls import reverse
+from django.db.models import Q
 
-# Create your models here.
+class Course(models.Model):
+
+	term = models.CharField(max_length=100)
+	class_type = models.CharField(max_length=100)
+	course = models.CharField(max_length=100)
+	section = models.CharField(max_length=100)
+	course_title = models.CharField(max_length=100)
+	instructor_first_name = models.CharField(max_length=100)
+	instructor_last_name = models.CharField(max_length=100)
+	room_name = models.CharField(max_length=100)
+	author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=None)
+	timeslot = models.CharField(max_length=100)
+	max_enroll = models.IntegerField()
+	room_size = models.IntegerField()
+	num_tas = models.IntegerField()
+	description = models.TextField()
+
+	def __str__(self):
+		return self.course_title
+	
+	def get_absolute_url(self):
+		return reverse('home')
