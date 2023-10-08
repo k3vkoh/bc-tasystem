a47dbe8d5ce721f3ab6f6df60cad9b22f271a9fe
Andy Deng <denga@bc.edu>
2023-10-08 00:04:43 -0400

    application list view

--- src/applications/urls.py
+++ src/applications/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ApplicationCreateView
-
+from .views import ApplicationCreateView, ApplicationListView
 app_name = 'applications'
 
 urlpatterns = [
     path('create/<int:pk>/', ApplicationCreateView.as_view(), name='application-create'),
+    path('', ApplicationListView.as_view(), name='application-list'),
 ]
--- src/applications/views.py
+++ src/applications/views.py
@@ -8,7 +8,7 @@ from .forms import ApplicationForm
 
 class ApplicationCreateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Application
-    template_name = 'application.html'
+    template_name = 'application_form.html'
     form_class = ApplicationForm
     success_message = "Your application has been submitted successfully!"
 
@@ -30,6 +30,28 @@ class ApplicationCreateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesT
     
     def get_success_url(self):
         return self.get_object().get_absolute_url()
+    
+
+class ApplicationListView(LoginRequiredMixin, ListView):
+    model = Application
+    template_name = 'applications.html'
+    context_object_name = 'applications'
+    
+    def get_queryset(self):
+        return Application.objects.filter(student=self.request.user)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_student():
+            context['title'] = 'My Applications'
+            context['applications'] = self.get_queryset().filter(student=self.request.user)
+        else:
+            context['title'] = 'Applications'
+            context['applications'] = self.get_queryset().filter(course__professor=self.request.user)
+        
+        return context
+    
+
     
     
 
