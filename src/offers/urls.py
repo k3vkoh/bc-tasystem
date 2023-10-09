f7a3bc18d29ddfcb2452fc365b638be162a031e9
Andy Deng <denga@bc.edu>
2023-10-08 21:45:55 -0400

    added detail, delete, and list views for offers

--- src/offers/urls.py
+++ src/offers/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import OfferCreateView
+from .views import OfferCreateView, OfferListView, OfferDeleteView, OfferDetailView
 
 app_name = 'offers'
 
 urlpatterns = [
     path('create/<int:pk>/', OfferCreateView.as_view(), name='offer-create'),
+    path('', OfferListView.as_view(), name='offer-list'),
+    path('delete/<int:pk>/', OfferDeleteView.as_view(), name='offer-delete'),
+    path('detail/<int:pk>/', OfferDetailView.as_view(), name='offer-detail'),
 ]
--- src/offers/views.py
+++ src/offers/views.py
@@ -5,6 +5,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from applications.models import Application
 from courses.models import Course
 from .models import Offer
+from django.urls import reverse
 
 
 class OfferCreateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, CreateView):
@@ -18,6 +19,8 @@ class OfferCreateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMix
         form.instance.recipient = self.get_object().student
         form.instance.course = self.get_object().course
         form.instance.application = self.get_object()
+
+        self.get_object().accept()
 
         return super().form_valid(form)
     
@@ -33,4 +36,56 @@ class OfferCreateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMix
         return context
     
     def get_success_url(self):
-        return self.get_object().get_absolute_url()
+        return reverse("offers:offer-list")
+    
+class OfferListView(LoginRequiredMixin, ListView):
+    model = Offer
+    template_name = 'offers.html'
+    context_object_name = 'offers'
+    
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Offer.objects.all()
+        elif self.request.user.is_student():
+            return Offer.objects.filter(recipient=self.request.user)
+        else:
+            return Offer.objects.filter(course__professor=self.request.user)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['offers'] = self.get_queryset()
+        if self.request.user.is_student():
+            context['title'] = 'My Offers'
+        else:
+            context['title'] = 'Offers'
+        return context
+    
+class OfferDeleteView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Offer
+    success_message = "Your offer has been deleted successfully!"
+    template_name = 'offer_confirm_delete.html'
+
+    
+    def get_success_url(self):
+        return reverse('offers:offer-list')
+    
+    def test_func(self):
+        return self.get_object().sender == self.request.user or self.request.user.is_superuser
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+class OfferDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Offer
+    template_name = 'offer_detail.html'
+    
+    def test_func(self):
+        return self.get_object().recipient == self.request.user or self.request.user.is_superuser
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['application'] = self.get_object().application
+        return context
+    
+
