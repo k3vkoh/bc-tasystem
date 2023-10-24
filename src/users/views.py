d695faeb1f9368c50abf3c64103336b5c028c7c5
Andy Deng <denga@bc.edu>
2023-10-24 17:10:20 -0400

    Remove unneeded user creation code

--- src/users/views.py
+++ src/users/views.py
@@ -1,22 +1,7 @@
-from django.contrib.auth import login
-from django.contrib import messages
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView, UpdateView
-from .forms import CustomUserCreationForm, CustomUserUpdateForm
+from django.views.generic.edit import UpdateView
+from .forms import CustomUserUpdateForm
 from .models import CustomUser
-
-
-class RegisterView(CreateView):
-    template_name = 'register.html'
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('home')
-
-    def form_valid(self, form):
-        user = form.save()
-        login(self.request, user)
-        messages.success(self.request, 'Account created successfully')
-        return super().form_valid(form)
 
 
 class ProfileView(UpdateView):
